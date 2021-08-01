f307a42546c14d772463aa0db9b0d624ff8beae9
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2021-08-01 23:30:33 +0000

    make pf public (undocumented tho)

--- commands/music/playfile.js
+++ commands/music/playfile.js
@@ -3,25 +3,35 @@ const discord = require("discord.js");
 const https = require("https");
 const fs = require("fs");
 exports.info = {
-  name: "playfile",
-  alts: ["pf"],
-  description: "Dev only",
+	name: "playfile",
+	alts: ["pf"],
+	description: "Dev only",
 };
 
-exports.run = async function (bot, msg, args, prefix) {
-  if (!botdevs.includes(msg.author.id)) {
-    return msg.channel.send(
-      "Sorry but this feature is dev only at the moment!"
-    );
-  }
-  if (!args[1]) return msg.channel.send("Please provide a file URL!");
-  const connection = await msg.member.voice.channel.join();
-  const date = Date.now();
-  var file = await fs.createWriteStream(`./audio/${date}.wav`);
-  const response = await https.get(args[1], async function (response) {
-    await response.pipe(file);
-    response.on("end", () => {
-      connection.play(`./audio/${date}.wav`);
-    });
-  });
+exports.run = async function(bot, msg, args, prefix) {
+	if (
+		!botdevs.includes(msg.author.id) &&
+		!msg.member.hasPermission("ADMINISTRATOR")
+	) {
+		return msg.channel.send(
+			"Sorry but this feature is dev only at the moment!"
+		);
+	}
+	if (!args[1]) return msg.channel.send("Please provide a file URL!");
+	const vc = await msg.member.voice.channel
+	const connection = await vc.join();
+	const date = Date.now();
+	var file = await fs.createWriteStream(`./audio/${date}.wav`);
+	const response = await https.get(args[1], async function(response) {
+		await response.pipe(file);
+		response.on("end", async () => {
+			const dispatcher = connection.play(`./audio/${date}.wav`);
+			const message = await msg.channel.send("Playing file!");
+			dispatcher.on("finish", () => {
+				fs.unlinkSync(`./audio/${date}.wav`);
+				message.edit("Song has finished playing!");
+				vc.leave();
+			});
+		});
+	});
 };
