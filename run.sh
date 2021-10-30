dff93fce6dfe9da4675dec0226b3ecf30a1099cc
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2021-10-30 14:41:12 -0500

    add rob command

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+# IMPORTANT
+# When running in production it is suggested to add a --no-check before index.ts
+# This prevents deno from type checking the files in a production enviroment
+
 # Launch Lavalink with the bot
 deno run --import-map=imports.json --allow-net --allow-env --allow-read --allow-write --allow-run index.ts
 
