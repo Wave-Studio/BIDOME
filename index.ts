122d05af974ed124ca07abb47d48c986d7ace3b6
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2021-12-10 15:52:56 -0600

    Prevent bot crash

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import { CommandClient, GatewayIntents, CommandContext, Embed } from 'harmony';
-import { Database, initDatabases } from 'database';
-import { getRandomStatus } from 'status';
+import { CommandClient, GatewayIntents, CommandContext, Embed } from "harmony";
+import { Database, initDatabases } from "database";
+import { getRandomStatus } from "status";
 
-import 'https://deno.land/x/dotenv@v3.0.0/load.ts';
+import "https://deno.land/x/dotenv@v3.0.0/load.ts";
 
-if (!Deno.args.includes('--no-lava')) {
+if (!Deno.args.includes("--no-lava")) {
 	Deno.run({
-		cmd: ['java', '-jar', 'lavalink/Lavalink.jar'],
+		cmd: ["java", "-jar", "lavalink/Lavalink.jar"],
 	});
 } else {
 	console.log("Flag '--no-lava' detected, not launching lavalink");
@@ -18,10 +18,10 @@ const bot = new CommandClient({
 	prefix: [],
 	async getGuildPrefix(guildid: string): Promise<string> {
 		if (isProdTesting) return "<@!778670182956531773>";
-		let prefix = (await Database.get('prefix.' + guildid)) as string;
-		if (typeof prefix === 'undefined') {
-			prefix = '!';
-			await Database.set('prefix.' + guildid, prefix);
+		let prefix = (await Database.get("prefix." + guildid)) as string;
+		if (typeof prefix === "undefined") {
+			prefix = "!";
+			await Database.set("prefix." + guildid, prefix);
 		}
 		return prefix;
 	},
@@ -31,26 +31,26 @@ const bot = new CommandClient({
 	caseSensitive: false,
 	presence: {
 		activity: {
-			name: 'Bidome Bot | Starting up',
-			type: 'PLAYING',
+			name: "Bidome Bot | Starting up",
+			type: "PLAYING",
 		},
-		status: 'idle',
+		status: "idle",
 	},
 	enableSlash: false,
 	spacesAfterPrefix: true,
-	owners: ['314166178144583682', '423258218035150849'],
+	owners: ["314166178144583682", "423258218035150849"],
 });
 
-bot.on('ready', async () => {
+bot.on("ready", async () => {
 	console.log(`Logged in as ${bot.user?.tag}`);
-	console.log('Loading Database');
+	console.log("Loading Database");
 	initDatabases();
-	console.log('Loading all commands!');
-	for await (const commands of Deno.readDir('./commands/')) {
-		if (commands.name.startsWith('-')) continue;
+	console.log("Loading all commands!");
+	for await (const commands of Deno.readDir("./commands/")) {
+		if (commands.name.startsWith("-")) continue;
 		if (
 			commands.isFile &&
-			(commands.name.endsWith('.ts') || commands.name.endsWith('.tsx'))
+			(commands.name.endsWith(".ts") || commands.name.endsWith(".tsx"))
 		) {
 			bot.commands.add((await import(`./commands/${commands.name}`)).command);
 		} else {
@@ -59,9 +59,9 @@ bot.on('ready', async () => {
 			)) {
 				if (
 					subcommand.isFile &&
-					(subcommand.name.endsWith('.ts') || subcommand.name.endsWith('.tsx'))
+					(subcommand.name.endsWith(".ts") || subcommand.name.endsWith(".tsx"))
 				) {
-					if (subcommand.name.startsWith('-')) continue;
+					if (subcommand.name.startsWith("-")) continue;
 					bot.commands.add(
 						(await import(`./commands/${commands.name}/${subcommand.name}`))
 							.command
@@ -71,12 +71,12 @@ bot.on('ready', async () => {
 		}
 	}
 
-	for await (const extension of Deno.readDir('./extensions')) {
+	for await (const extension of Deno.readDir("./extensions")) {
 		if (
 			extension.isFile &&
-			(extension.name.endsWith('.ts') || extension.name.endsWith('.tsx'))
+			(extension.name.endsWith(".ts") || extension.name.endsWith(".tsx"))
 		) {
-			if (extension.name.startsWith('-')) continue;
+			if (extension.name.startsWith("-")) continue;
 			bot.extensions.load(
 				(await import(`./extensions/${extension.name}`)).extension
 			);
@@ -85,34 +85,42 @@ bot.on('ready', async () => {
 
 	console.log(
 		`Loaded ${await bot.commands.list.size} command${
-			bot.commands.list.size == 1 ? '' : 's'
+			bot.commands.list.size == 1 ? "" : "s"
 		} and ${await bot.extensions.list.size} extension${
-			bot.extensions.list.size == 1 ? '' : 's'
+			bot.extensions.list.size == 1 ? "" : "s"
 		}!`
 	);
-	console.log('Loaded bot!');
+	console.log("Loaded bot!");
 
 	setInterval(() => {
 		nextStatus();
 	}, 30000);
 });
 
-bot.on('commandError', async (ctx: CommandContext, err: Error) => {
+bot.on("commandError", async (ctx: CommandContext, err: Error) => {
 	console.log(
 		`An error occured while executing ${ctx.command.name}! Here is the stacktrace:`
 	);
 	console.log(err);
-	await ctx.message.reply(undefined, {
-		embed: new Embed({
-			author: {
-				name: 'Bidome bot',
-				icon_url: ctx.message.client.user?.avatarURL(),
-			},
-			title: 'An error occured!',
-			description:
-				'An error occured while executing this command! If this command continues erroring please alert a developer!',
-		}).setColor('random'),
-	});
+	try {
+		await ctx.message.reply(undefined, {
+			embed: new Embed({
+				author: {
+					name: "Bidome bot",
+					icon_url: ctx.message.client.user?.avatarURL(),
+				},
+				title: "An error occured!",
+				description:
+					"An error occured while executing this command! If this command continues erroring please alert a developer!",
+			}).setColor("random"),
+		});
+	} catch {
+		try {
+			await ctx.message.addReaction("❗");
+		} catch {
+			return;
+		}
+	}
 });
 
 const nextStatus = async () => {
@@ -122,7 +130,7 @@ const nextStatus = async () => {
 			name,
 			type,
 		},
-		status: status ?? 'idle',
+		status: status ?? "idle",
 	});
 };
 
@@ -130,7 +138,7 @@ const nextStatus = async () => {
 // not being ready
 setTimeout(
 	() => {
-		bot.connect(Deno.env.get('token'), [
+		bot.connect(Deno.env.get("token"), [
 			GatewayIntents.GUILDS,
 			GatewayIntents.GUILD_MESSAGES,
 			GatewayIntents.GUILD_VOICE_STATES,
@@ -139,5 +147,5 @@ setTimeout(
 		]);
 	},
 	// Prevent users from waiting when lavalink isn't being launched
-	Deno.args.includes('--no-lava') ? 1 : 1.5 * 1000
+	Deno.args.includes("--no-lava") ? 1 : 1.5 * 1000
 );
