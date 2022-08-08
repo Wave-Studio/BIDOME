83200db9ecdcc9672d83ebb2554fa840c0fad73b
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2022-08-08 17:34:54 -0500

    Update nodes.ts

--- imports/nodes.ts
+++ imports/nodes.ts
@@ -1,9 +1,12 @@
 import { ClusterNodeOptions } from "./lavadeno.ts";
 
+const envfile = (await Deno.readTextFile(".env")).split("\n");
+
 export const nodes: ClusterNodeOptions[] = [{
-	host: Deno.env.get("LAVALINK_HOST")!,
-	port: parseInt(Deno.env.get("LAVALINK_PORT")!),
-	password: Deno.env.get("LAVALINK_PASSWORD")!,
+	// Me when env isn't loading correctly
+	host: envfile.filter((l) => l.startsWith("LAVALINK_HOST"))[0].substring("LAVALINK_HOST=".length),
+	port: parseInt(envfile.filter((l) => l.startsWith("LAVALINK_PORT"))[0].substring("LAVALINK_PORT=".length)),
+	password: envfile.filter((l) => l.startsWith("LAVALINK_PASSWORD"))[0].substring("LAVALINK_PASSWORD=".length),
 	id: "1",
 	reconnect: {
 		type: "basic",
