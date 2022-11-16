4dcc729d93d6cd7b844796a0122409026011398c
Lukas <69743171+quick007@users.noreply.github.com>
2022-11-15 16:53:01 -0800

    Update calculator.tsx

--- commands/fun/calculator.tsx
+++ commands/fun/calculator.tsx
@@ -11,8 +11,8 @@ import {
 
 export default class Calculator extends Command {
 	name = "calculator";
-	description = 'Who doesn\'t like a good ol "MTX 84 Ti 16gb"';
-	aliases = ["calc", "84ti", "shitty84ti", "math"];
+	description = 'Who doesn\'t like a good ol "MTX 8040 Tl 16gb"';
+	aliases = ["calc", "8040tl", "shitty84tl", "math"];
 	usage = "calculator";
 	category = "fun";
 	async execute(ctx: CommandContext) {
@@ -23,7 +23,7 @@ export default class Calculator extends Command {
 						name: "Bidome bot",
 						icon_url: ctx.message.client.user!.avatarURL(),
 					},
-					title: '"Ti 84"',
+					title: '"MTX 8040 Tl 16gb"',
 					description: "```\nPress any button\n```",
 					fields: [
 						{
