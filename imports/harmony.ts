3be0b1ba059e0c323d44524d2e4dc1b5b81e2dc6
Blocks <51055767+Blocksnmore@users.noreply.github.com>
2023-05-30 14:28:09 -0500

    Update harmony to prevent crash
    Fixed in https://github.com/harmonyland/harmony/issues/351

--- imports/harmony.ts
+++ imports/harmony.ts
@@ -1,4 +1,4 @@
-export * from "https://raw.githubusercontent.com/harmonyland/harmony/daca400ae9feab19604381abddbdab16aa1ede2b/mod.ts";
+export * from "https://raw.githubusercontent.com/harmonyland/harmony/5d3cf6dff35ed56df91ef55a084be0508f23b309/mod.ts";
 import {
 	ApplicationCommandInteraction,
 	ApplicationCommandPartial,
