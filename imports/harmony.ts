3634a7653255e2ebbe4edb5c78a82b3f341af1e9
Blocks <51055767+Blocksnmore@users.noreply.github.com>
2023-06-08 20:52:04 -0500

    Forgor to update

--- imports/harmony.ts
+++ imports/harmony.ts
@@ -1,4 +1,4 @@
-//export * from "https://raw.githubusercontent.com/harmonyland/harmony/5d3cf6dff35ed56df91ef55a084be0508f23b309/mod.ts";
+export * from "https://raw.githubusercontent.com/harmonyland/harmony/5d3cf6dff35ed56df91ef55a084be0508f23b309/mod.ts";
 import {
 	ApplicationCommandInteraction,
 	ApplicationCommandPartial,
@@ -9,4 +9,4 @@ export interface ApplicationCommand extends ApplicationCommandPartial {
 }
 
 // For local harmony development:
-export * from "../../harmony/mod.ts";
+//export * from "../../harmony/mod.ts";
